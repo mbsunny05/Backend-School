fb66751a0c7e2453c3b9d927ebccc93dffc7a191
MBSUNNY <mayurbachhav88@gmail.com>
2026-02-03 23:34:57 +0530

    changes

--- utils/db.js
+++ utils/db.js
@@ -1,10 +1,14 @@
-const mysql2=require('mysql2')
+const mysql = require('mysql2');
 
-const pool=mysql2.createPool({
-    host:'localhost',
-    user:'root',
-    password:'pass123',
-    database:'school_Management_db'
-})
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
 
-module.exports=pool
+module.exports = pool;
